2b657b3eb082d26cea593cfdc979f3473c27292c
Awadh-Awadh <awadhawadh84@yahoo.com>
2021-12-16 15:09:05 +0300

    add logic to keep track of the scores

--- question_model.py
+++ question_model.py
@@ -2,7 +2,7 @@ class Question:
 
   def __init__(self, question, answer):
       self.question = question
-      self.anser = answer
-      
+      self.answer = answer
+
   def __str__(self) -> str:
       self.question
